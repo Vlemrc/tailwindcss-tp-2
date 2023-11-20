33dfec52d81c1034c556e9894e5980f0b2e74551
Vlemrc <lemerciervictor@gmail.com>
2023-11-20 15:57:11 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1C85E8",
+      },
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '3rem',
+        '6xl': '4rem',
+      },
+      fontSize: {
+        '7xl': '5rem',
+        '8xl': '6rem',
+      }
+    },
+  },
+  plugins: [],
+}
+
